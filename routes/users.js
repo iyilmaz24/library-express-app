6418abbf1f175a5694b8c29d8450f4b8122f163f
Irfan Yilmaz <iy.usa@outlook.com>
2024-02-04 21:33:10 -0500

    change var to const in app.js, add users/demo route

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/demo', function(req, res, next) {
+  const content = "demo page, url: "  + req.url;
+  res.render("index", { title: content });
+});
+
 module.exports = router;
